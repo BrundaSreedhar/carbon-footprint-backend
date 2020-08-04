1b453a6bbc9c6b512bd95bb7f71c2aa628fcdf05
BrundaSreedhar <brundasreedhar@gmail.com>
2020-08-04 21:50:11 +0530

    test

--- js/app.js
+++ js/app.js
@@ -22,7 +22,7 @@ app.post('/getAirportList', async (req, res) => {
     res.send(list);
 });
 
-app.get("/hi", (req, res) => {
+app.get("/api/hi", (req, res) => {
     res.send("hi!");
 });
 app.post('/getDistance', (req, res) => {
